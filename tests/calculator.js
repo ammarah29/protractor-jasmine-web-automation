beb43d20a4cd533f0ed0a167ddfadfc52269348d
ammarah29 <ammarahahmad@hotmail.com>
2021-12-05 21:07:21 +0000

    add js files

---
+++ tests/calculator.js
@@ -0,0 +1,27 @@
+let homepage = require('../pages/homepage');
+
+describe('demo calc test', function(){
+
+        it('addition test', function(){
+
+          //browser.get('http://juliemr.github.io/protractor-demo/');
+          homepage.get('http://juliemr.github.io/protractor-demo/');
+
+          //element(by.model('first')).sendKeys('2');
+          homepage.enterFirstNumber('4');
+
+          //element(by.model('second')).sendKeys('3');
+          homepage.enterSecondNumber('3');
+
+          //element(by.css('[ng-click="doAddition()"]')).click();
+          homepage.clickGo();
+
+          //let result = element(by.cssContainingText('.ng-binding', '5'));
+          //expect(result.getText()).toEqual('5');
+
+          homepage.verfiyResult('7');
+
+          browser.sleep(2000)
+
+        });
+});
